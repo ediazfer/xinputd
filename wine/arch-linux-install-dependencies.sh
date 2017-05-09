ad66cbafeddd5572b35d3e72b3570c55cf4895a5
ediazfer <github@hurrikhan.eu>
2017-05-09 21:12:13 +0200

    install dependencies for arch-linux / manjaro / manjaro openrc
    	new file:   arch-linux-install-dependencies.sh

---
+++ wine/arch-linux-install-dependencies.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+
+pacman -Ss nosystemd > /dev/null 2>&1
+
+# try to detect systemd and act on it
+
+if [ $? -eq 0 ]
+then
+    N=-nosystemd
+fi
+
+# try to detect multilib and act on it
+
+pacman -Ql gcc-libs-multilib > /dev/null 2>&1
+if [ $? -eq 0 ]
+then
+    M=-multilib
+fi
+
+deps="
+  attr             lib32-attr
+  fontconfig       lib32-fontconfig
+  lcms2            lib32-lcms2
+  libxml2          lib32-libxml2
+  libxcursor       lib32-libxcursor
+  libxrandr        lib32-libxrandr
+  libxdamage       lib32-libxdamage
+  libxi            lib32-libxi
+  gettext          lib32-gettext
+  freetype2        lib32-freetype2
+  glu              lib32-glu
+  libsm            lib32-libsm
+  gcc-libs$M       lib32-gcc-libs
+  libpcap          lib32-libpcap
+  desktop-file-utils
+"
+
+make_deps="
+ autoconf ncurses bison perl fontforge flex
+  gcc$M              gcc-multilib
+  giflib                lib32-giflib
+  libpng                lib32-libpng
+  gnutls                lib32-gnutls
+  libxinerama           lib32-libxinerama
+  libxcomposite         lib32-libxcomposite
+  libxmu                lib32-libxmu
+  libxxf86vm            lib32-libxxf86vm
+  libldap               lib32-libldap
+  mpg123                lib32-mpg123
+  openal                lib32-openal
+  v4l-utils             lib32-v4l-utils
+  alsa-lib              lib32-alsa-lib
+  libxcomposite         lib32-libxcomposite
+  mesa                  lib32-mesa
+  mesa-libgl            lib32-mesa-libgl
+  opencl-icd-loader     lib32-opencl-icd-loader
+  libxslt               lib32-libxslt
+  libpulse              lib32-libpulse
+  libva                 lib32-libva
+  gtk3                  lib32-gtk3
+  gst-plugins-base-libs lib32-gst-plugins-base-libs
+  samba
+  opencl-headers
+"
+
+opt_deps="
+  giflib                lib32-giflib
+  libpng                lib32-libpng
+  libldap               lib32-libldap
+  gnutls                lib32-gnutls
+  mpg123                lib32-mpg123
+  openal                lib32-openal
+  v4l-utils             lib32-v4l-utils
+  libpulse              lib32-libpulse
+  alsa-plugins          lib32-alsa-plugins
+  alsa-lib              lib32-alsa-lib
+  libjpeg-turbo         lib32-libjpeg-turbo
+  libxcomposite         lib32-libxcomposite
+  libxinerama           lib32-libxinerama
+  ncurses               lib32-ncurses
+  opencl-icd-loader     lib32-opencl-icd-loader
+  libxslt               lib32-libxslt
+  libva                 lib32-libva
+  gtk3                  lib32-gtk3
+  gst-plugins-base-libs lib32-gst-plugins-base-libs
+  vulkan-icd-loader     lib32-vulkan-icd-loader
+  cups$N
+  samba           dosbox
+  "
+
+# on my machine, this does not install anything else (Manjaro-OpenRC)
+# which is as it should be
+
+pacman -Su --needed $deps $make_deps $opt_deps
+
